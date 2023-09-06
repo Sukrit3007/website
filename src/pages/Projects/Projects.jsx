d551be93fbfd845111584ce90a4a22a91ccb405d
Sukrit <sukrit3007@outlook.com>
2023-09-06 12:55:22 +0530

    finally fixed

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -9,7 +9,7 @@ function Projects(props) {
         <div key={index} className='projects-div'>
           <h1 className='projects-h1'>{project.heading}</h1>
           <p className='projects-p'>{project.description}</p>
-          <button className='projects-button'><a className='projects-a' href={project.InfoLink} target="_blank">Source↗</a></button>
+          <button className='projects-button'><a className='projects-a' href={project.InfoLink} target="_blank">Source↗ </a></button>
           <button className='projects-button'><a className='projects-a' href={project.DemoLink} target="_blank">Live Demo↗</a></button>
         </div>
       ))}
